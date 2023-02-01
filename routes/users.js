1af36b2e64517f09fe1bf8787fda1de74e84297f
linlinOvO <90375974+linlinOvO@users.noreply.github.com>
2023-02-01 20:11:33 +0000

    update GET@(users)

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(
+      JSON.stringify({message:"success"})
+  )
 });
 
 module.exports = router;
