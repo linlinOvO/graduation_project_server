84305d7fff41f260f13b1b8c0cd647722624e8ab
linlinOvO <90375974+linlinOvO@users.noreply.github.com>
2023-02-20 07:09:53 +0000

    debug for changing host

--- routes/database.js
+++ routes/database.js
@@ -2,12 +2,12 @@ const {createPool} = require('mysql');
 
 // 要连接的数据库
 const pool = createPool({
-    host: "localhost",
+    host: "127.0.0.1",
     port: 3306, // default port for mysql is 3306
     user: "root",
     password: "password",
     connectionLimit: 10,
-    multipleStatements: true
+    multipleStatements: true,
 });
 
 module.exports = pool
